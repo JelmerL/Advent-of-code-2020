1200b6f72a727a43f4cf206dc9c015211fc3a388
Jelmer <jelmerlaks@gmail.com>
2020-12-11 13:20:18 -0500

    read in logic

--- Advent of Code Day 4.py
+++ Advent of Code Day 4.py
@@ -3,6 +3,13 @@ single = []
 #read in the source file
 with open("input4.txt", "r") as f:
     for line in f:
-        entry.append(line)
+        line = line.replace("\n","")
+        if line!='\n':
+            single.append(line)
+        if line=='':
+            entry.append(single)
+            single =[]
 
-print(entry)
+for count, item in enumerate(entry):
+    print(type(entry))
+print(entry[2])
