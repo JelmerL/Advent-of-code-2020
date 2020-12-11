d00154ac19619732b1e98e19e96a82acc4f6f5a1
Jelmer <jelmerlaks@gmail.com>
2020-12-11 12:18:43 -0500

    commit on master

--- Advent of Code Day 4.py
+++ Advent of Code Day 4.py
@@ -1,12 +1,7 @@
 entry = []
 single = []
 with open("input4.txt", "r") as f:
-    for count, line in enumerate(f):
-        while line != '\n':
-            single.append(line)
-        while line == '\n':
-            entry.append(single)
-            single = []
-            break
+    for line in f:
+        entry.append(line)
 
-print(count)
+print(entry)
